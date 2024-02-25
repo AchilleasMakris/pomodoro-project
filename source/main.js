9b971085d1cf566f541339910dc6a7cbd034d799
Lexakozz <lexlarisa@hotmail.com>
2024-02-25 21:26:56 +0200

    first commit

--- source/main.js
+++ source/main.js
@@ -1,12 +1,11 @@
 import '../styles/style.css';
 import '../styles/components/header.css';
 import '../styles/components/hero.css';
-import '../styles/components/about.css';
+
 import '../styles/components/featured.css';
-import '../styles/components/work.css';
-import '../styles/components/contact.css';
+
 import '../styles/components/footer.css';
-import '../styles/components/mobile-nav.css';
+
 import '../styles/utils.css';
 
 
