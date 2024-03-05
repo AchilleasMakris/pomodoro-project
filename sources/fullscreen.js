ccc57eb970bd5eca0ed7d21400bdb12f945f8d03
Achilleas Makris <lexlarisa@hotmail.com>
2024-03-05 11:00:06 +0200

    mobile test

--- sources/fullscreen.js
+++ sources/fullscreen.js
@@ -3,15 +3,25 @@ const fullscreenBtn = document.getElementById("fullscreen-btn");
 fullscreenBtn.addEventListener("click", toggleFullScreen);
 
 function toggleFullScreen() {
-  if (!document.fullscreenElement) {
-    document.documentElement.requestFullscreen().catch((err) => {
-      console.error(
-        `Error attempting to enable full-screen mode: ${err.message} (${err.name})`
-      );
-    });
+  if (!document.fullscreenElement && !document.webkitFullscreenElement) {
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen().catch((err) => {
+        console.error(
+          `Error attempting to enable full-screen mode: ${err.message} (${err.name})`
+        );
+      });
+    } else if (document.documentElement.webkitRequestFullscreen) {
+      document.documentElement.webkitRequestFullscreen().catch((err) => {
+        console.error(
+          `Error attempting to enable full-screen mode: ${err.message} (${err.name})`
+        );
+      });
+    }
   } else {
     if (document.exitFullscreen) {
       document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
     }
   }
 }
